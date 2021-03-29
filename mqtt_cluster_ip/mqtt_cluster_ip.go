0bf409b2ca2773c8da1a80ee0feb7d4db3d88ddf
wjwzero <13602573297@163.com>
2021-03-29 16:31:01 +0800

    1. 获取cluster ip 等待逻辑

--- mqtt_cluster_ip/mqtt_cluster_ip.go
+++ mqtt_cluster_ip/mqtt_cluster_ip.go
@@ -6,28 +6,32 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"time"
 )
 
 /**
 从南向获得分配到的mqtt节点
 */
 func GetMqttClusterIp(southUrl string, deviceKey string) string {
+BEGIN:
 	resp, err := http.Get(southUrl + "&deviceKey=" + deviceKey)
 	if err != nil {
 		log.Print("访问中心节点失败", err)
 	}
 	body, _ := ioutil.ReadAll(resp.Body)
 	str2 := string(body)
-	fmt.Println(str2)
-	json, _ := simplejson.NewJson(body)
-	code, _ := json.Get("code").Int()
+	fmt.Printf("body http %s", str2)
+	json, err := simplejson.NewJson(body)
+	code, err := json.Get("code").Int()
 	log.Print(code)
 	var res string
 	if code == 200 {
 		res, _ = json.Get("data").Get("ip").String()
 	} else {
 		res, _ = json.Get("data").String()
+		log.Printf("获得cluster异常%s", res)
+		time.Sleep(1 * time.Second)
+		goto BEGIN
 	}
-	log.Print(res)
 	return res
 }
